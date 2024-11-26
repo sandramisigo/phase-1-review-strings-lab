1e23e5f6c488259b10286b7f2bd807fda3880e32
Sandra Misigo <misigosandra@gmail.com>
2024-11-26 19:45:06 +0300

    mystringslab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
